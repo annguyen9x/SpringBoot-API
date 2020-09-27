48ff4ae054973d8537186f6276d76a04ed3a8168
An Nguyen <annguyen.htvn@gmail.com>
2020-09-27 15:55:53 +0700

    create project

---
+++ README.md
@@ -0,0 +1,9 @@
+# <p align="center">RESTfull API WebService</p>
+
+# Giới thiệu
+- Demo Restfull API WebService cho ứng dụng ...
+- Kết nối, thao tác với CSDL MySQL bằng Spring Data JPA trong Spring boot
+
+# Các chức năng
+## 1. Chức năng thêm, sửa, xóa bằng Spring Data JPA trong Spring Boot
+## 2. Chức năng hiển thị và phân trang bằng Spring Data JPA trong Spring Boot
---
+++ src/main/java/com/laptrinhjavaweb/api/NewAPI.java
@@ -0,0 +1,14 @@
+package com.laptrinhjavaweb.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class NewAPI {
+	
+	@GetMapping("/api/test")
+    public ResponseEntity<String> testSpringBoot() {
+        return ResponseEntity.ok("Success");
+    }
+}
