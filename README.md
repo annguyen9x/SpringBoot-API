ac53dc034bcae38d5e37549f9611a4f98df26d83
An Nguyen <annguyen.htvn@gmail.com>
2020-09-27 17:59:47 +0700

    test API

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # <p align="center">RESTfull API WebService</p>
 
 # Giới thiệu
-- Demo Restfull API WebService cho ứng dụng ...
+- Demo Restfull API Web Service cho ứng dụng Quản lý bài viết
 - Kết nối, thao tác với CSDL MySQL bằng Spring Data JPA trong Spring boot
 
 # Các chức năng
--- src/main/java/com/laptrinhjavaweb/api/NewAPI.java
+++ src/main/java/com/laptrinhjavaweb/api/NewAPI.java
@@ -1,14 +1,20 @@
 package com.laptrinhjavaweb.api;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.laptrinhjavaweb.dto.NewDTO;
 
 @Controller
 public class NewAPI {
 	
-	@GetMapping("/api/test")
-    public ResponseEntity<String> testSpringBoot() {
-        return ResponseEntity.ok("Success");
-    }
+	  @PostMapping(value = "/new")
+	  @ResponseBody
+	   public NewDTO createNew(@RequestBody NewDTO model) {
+	      return model;
+	   }
 }
